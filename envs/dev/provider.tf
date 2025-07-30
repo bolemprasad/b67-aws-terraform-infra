ed1e57f65181a4fba5bca877032f4c5d1307a90c
Pruthvi <Pruthvi@gmail.com>
2025-07-30 22:53:11 +0530

    Updated provider.tf for dev environment

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -5,13 +5,12 @@ provider "aws" {
 
 #stroing terraform state file in s3 bucket
 
-terraform {
-  backend "s3" {
-    bucket = "b67-jhc-bucket"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    #dynamodb_table = "terraform-locks"
-    encrypt = true
-    
-  }
-}
+ terraform {
+   backend "s3" {
+     bucket = "b67-jhc-bucket"
+     key    = "terraform.tfstate"
+     region = "us-east-1"
+     encrypt = true
+     dynamodb_table = "terraform-state-locks"
+   }
+ }
